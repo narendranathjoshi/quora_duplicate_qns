ac7204b0fc009a6c3e2fa5a910474119a7cdaab4
Narendra Nath Joshi <joshinarendranath@gmail.com>
2018-09-24 17:03:56 -0400

    Function to create embedding layer

--- classify.py
+++ classify.py
@@ -1,15 +1,30 @@
 import pandas
 import keras
 
+import preprocess
+
+MAX_SEQ_LENGTH = 100
+
+word2vec_model = preprocess.run()
+
 data_df = pandas.read_csv("data/train.csv")
 
-print("Loaded data into a DF")
+print("Loaded data into a dataframe")
 
-train_size = int(len(data_df) * .8)
- 
-train_X = data_df[['question1', 'question2']][:train_size]
-train_Y = data_df['is_duplicate'][:train_size]
- 
-val_X = data_df[['question1', 'question2']][train_size:]
-val_Y = data_df['is_duplicate'][train_size:]
 
+def create_embedding_layer():
+    vocab_size = len(word2vec_model.wv.vocab)
+    output_dim = len(word2vec_model[word2vec_model.wv.vocab[0]])
+    embedding_layer = keras.layers.Embedding(
+        vocab_size, output_dim, input_length=MAX_SEQ_LENGTH
+    )
+
+    return embedding_layer
+
+# train_size = int(len(data_df) * .8)
+
+# train_X = data_df[['question1', 'question2']][:train_size]
+# train_Y = data_df['is_duplicate'][:train_size]
+
+# val_X = data_df[['question1', 'question2']][train_size:]
+# val_Y = data_df['is_duplicate'][train_size:]
